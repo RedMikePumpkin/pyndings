a6b6ada769359c0e9ee8a7e2f0d880d815754a5a
RedMikePumpkin <michael@malinov.com>
2018-08-01 01:43:49 +0000

    Update helloworld.py

--- examples/helloworld.py
+++ examples/helloworld.py
@@ -1,21 +1,10 @@
 from pyndings import pynd, pprint
-from time import sleep
-import gpiozero
-
-button = gpiozero.Button(23)
 
 pprint("Hello, World!")
 
 def hello_again(msg):
 	pprint("hi.")
 	return "hello", msg
-
-def b():
-	while True:
-		button.wait_for_press()
-		pprint("pressed!")
-		button.wait_for_release()
-		pprint("released!")
 
 def goodbye(msg):
 	pprint("bye!")
