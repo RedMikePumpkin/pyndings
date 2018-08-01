a8b0d17ece4df35a444550e8aa16efabb84fac0a
RedMikePumpkin <michael@malinov.com>
2018-08-01 04:06:32 +0000

    fixed error

--- pyndings.js
+++ pyndings.js
@@ -1,30 +1,31 @@
-var spawn = require("child_process").spawn,
-	EventEmitter = require('events');
+var spawn = require("child_process").spawn;
 
 class python {
-	constructor (path, options = {py3: false}) {
+	constructor (path) {
 		this.path = path;
-		this._emitter = new EventEmitter();
-		this.py = spawn("python" + options.py3 ? "3" : "", [path]);
+		this.print = function() {};
+		this.return = function() {};
+		this.error = function() {};
+		this.py = spawn("python", [path]);
 		this.py.stdin.setEncoding("utf-8");
 		this.py.stdout.emitToPython = function(name, data) {
-			_this._emitter.emit(name, data)
+			_this[name](data);
 		}
 		var _this = this;
 		this.py.stdout.on('data', function(d) {
+			console.log("o");
 			var data = d.toString();
 			var ss = data.substring(0, 12);
-			var sp = data.slice(12, -1)
+			var sp = data.slice(12, -1);
 			if (ss === "~~RETURN~~: ") {
 				var type = sp.split(" ")[1];
 				type = type.slice(0, -1);
-				var val = sp.split(" ")
+				var val = sp.split(" ");
 				val = val.slice(2, val.length).join("");
 				this.emitToPython("return", [type, val]);
 			} else if (ss === "~~ERRORS~~: ") {
 				this.emitToPython("error", sp);
 			} else {
-				console.log(this == py.py.stdout)
 				this.emitToPython("print", data.trim());
 			}
 		});
@@ -39,15 +40,15 @@ class python {
 	}
 	
 	onPrint (callback) {
-		this._emitter.on("print", callback);
+		this.print = callback;
 	}
 	
 	onReturn (callback) {
-		this._emitter.on("return", callback);
+		this.return = callback;
 	}
 	
 	onError (callback) {
-		this._emitter.on("error", callback);
+		this.error = callback;
 	}
 }
 
