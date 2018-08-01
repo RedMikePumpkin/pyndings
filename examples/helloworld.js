1291e31644ba1f14f121132511f7e7b4414a1378
RedMikePumpkin <michael@malinov.com>
2018-08-01 01:43:21 +0000

    Create helloworld.js

---
+++ examples/helloworld.js
@@ -0,0 +1,26 @@
+var pyndings = require("pyndings");
+
+var pynding = new pyndings("./helloworld.py");
+pynding.onPrint((data) => {
+	console.log(data);
+});
+pynding.onReturn((data) => {
+	console.log(data + " returned");
+});
+pynding.onError((data) => {
+	throw new Error(data);
+});
+
+setTimeout(() => {
+	pynding.run("hello_again(\"Bob\")");
+	pynding.run("hello_again(\"Jane\")");
+	pynding.run("hello_again(\"Max\")");
+	setTimeout(() => {
+		pynding.run("goodbye(\"Bob\")");
+		pynding.run("goodbye(\"Jane\")");
+		pynding.run("goodbye(\"Max\")");
+		setTimeout(() => {
+			pynding.run("err()");
+		}, 2000);
+	}, 2000);
+}, 2000);
